7c7627642ec77e9f6c4c3e9744da192599845f3f
Robert Montgomery <rmontgomery@kumc.edu>
2022-10-19 14:33:55 -0500

    Fixing incorrect labeling

--- R/predplot.R
+++ R/predplot.R
@@ -19,10 +19,8 @@ predplot <- function(predictionresults = results){
   forplot$diff <- as.numeric(as.character(forplot$diff))
   forplot$resulted <- as.numeric(as.character(forplot$resulted))
 
-
   ymax <- max(1.25*max(forplot$diff), 0+.5*sd(forplot$diff))
   ymin <- min(1.25*min(forplot$diff), 0-.5*sd(forplot$diff))
-
 
   forplot1 <- ggplot(data = forplot, aes(end, diff, fill = factor(resulted)   )  ) +
     geom_bar(aes(x = end, y = diff),stat='identity') +
@@ -31,9 +29,8 @@ predplot <- function(predictionresults = results){
     theme_classic() +
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
     labs( x = "", y = "Difference", fill = "Prediction\nresults") +
-    scale_fill_manual(labels = c("Incorrect", "Correct"), values = c("White", "Black")) +
+    scale_fill_manual(values = c('0' = "White",'1'= "Black"), labels = c("Incorrect", "Correct"), drop = FALSE) +
     theme(plot.title = element_text(hjust = 0.5))
-
 
   return(forplot1)
 
