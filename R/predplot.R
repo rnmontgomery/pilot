d09d572e810ddccae04da5eaf355341229f6d360
Robert Montgomery <rmontgomery@kumc.edu>
2022-10-26 11:03:53 -0500

    Plot features (weights, order)

--- R/predplot.R
+++ R/predplot.R
@@ -9,20 +9,26 @@
 #' @import ggplot2
 #'
 #' @examples
-predplot <- function(predictionresults = results){
+predplot <- function(predictionresults = results, weights, order = FALSE){
 
-  end <- predictionresults[[3]]
-  diff <- unlist(predictionresults[2])
-  resulted <- unlist(predictionresults[1])
+  if (missing(weights))
+  {
 
-  forplot <- as.data.frame(   cbind(end, diff  , resulted  )   )
-  forplot$diff <- as.numeric(as.character(forplot$diff))
-  forplot$resulted <- as.numeric(as.character(forplot$resulted))
+    end <- predictionresults[[3]]
+    diff <- unlist(predictionresults[2])
+    resulted <- unlist(predictionresults[1])
 
-  ymax <- max(1.25*max(forplot$diff), 0+.5*sd(forplot$diff))
-  ymin <- min(1.25*min(forplot$diff), 0-.5*sd(forplot$diff))
+    forplot <- as.data.frame(   cbind(end, diff  , resulted  )   )
+    forplot$diff <- as.numeric(as.character(forplot$diff))
+    forplot$resulted <- as.numeric(as.character(forplot$resulted))
 
-  forplot1 <- ggplot(data = forplot, aes(end, diff, fill = factor(resulted)   )  ) +
+    ymax <- max(1.25*max(forplot$diff), 0+.5*sd(forplot$diff))
+    ymin <- min(1.25*min(forplot$diff), 0-.5*sd(forplot$diff))
+
+
+    if (order == FALSE){
+
+     forplot1 <- ggplot(data = forplot, aes(end, diff, fill = factor(resulted)   )  ) +
     geom_bar(aes(x = end, y = diff),stat='identity') +
     scale_y_continuous(limits=c(ymin,ymax)) +
     geom_bar(forplot, mapping = aes(end) ,alpha=0, size=1, color="black", stat='identity')+
@@ -32,6 +38,78 @@ predplot <- function(predictionresults = results){
     scale_fill_manual(values = c('0' = "White",'1'= "Black"), labels = c("Incorrect", "Correct"), drop = FALSE) +
     theme(plot.title = element_text(hjust = 0.5))
 
+    } else if (order == TRUE){
+
+
+      # This didn;t work
+      forplotO <- forplot[order(diff, decreasing = TRUE),]
+
+
+      forplot1 <- ggplot(data = forplot, aes(x=reorder(end, -diff), y=diff, fill = factor(resulted)   )  ) +
+        geom_bar(stat='identity') +
+        scale_y_continuous(limits=c(ymin,ymax)) +
+        geom_bar(forplot, mapping = aes(end) ,alpha=0, size=1, color="black", stat='identity')+
+        theme_classic() +
+        theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+        labs( x = "", y = "Difference", fill = "Prediction\nresults") +
+        scale_fill_manual(values = c('0' = "White",'1'= "Black"), labels = c("Incorrect", "Correct"), drop = FALSE) +
+        theme(plot.title = element_text(hjust = 0.5))
+
+
+    }
+  } else {
+
+    end <- predictionresults[[3]]
+    diff <- unlist(predictionresults[2])*unlist(wt1)
+    resulted <- unlist(predictionresults[1])
+
+    forplot <- as.data.frame(   cbind(end, diff  , resulted  )   )
+    forplot$diff <- as.numeric(as.character(forplot$diff))
+    forplot$resulted <- as.numeric(as.character(forplot$resulted))
+
+    ymax <- max(1.25*max(forplot$diff), 0+.5*sd(forplot$diff))
+    ymin <- min(1.25*min(forplot$diff), 0-.5*sd(forplot$diff))
+
+    if (order == FALSE)
+    {
+
+    forplot1 <- ggplot(data = forplot, aes(end, diff, fill = factor(resulted)   )  ) +
+      geom_bar(aes(x = end, y = diff),stat='identity') +
+      scale_y_continuous(limits=c(ymin,ymax)) +
+      geom_bar(forplot, mapping = aes(end) ,alpha=0, size=1, color="black", stat='identity')+
+      theme_classic() +
+      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+      labs( x = "", y = "Weighted difference", fill = "Prediction\nresults") +
+      scale_fill_manual(values = c('0' = "White",'1'= "Black"), labels = c("Incorrect", "Correct"), drop = FALSE) +
+      theme(plot.title = element_text(hjust = 0.5))
+    } else if (order == TRUE){
+
+      forplot1 <- ggplot(data = forplot, aes(x=reorder(end, - diff), y = diff, fill = factor(resulted)   )  ) +
+        geom_bar(stat='identity') +
+        scale_y_continuous(limits=c(ymin,ymax)) +
+        geom_bar(forplot, mapping = aes(end) ,alpha=0, size=1, color="black", stat='identity')+
+        theme_classic() +
+        theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+        labs( x = "", y = "Weighted difference", fill = "Prediction\nresults") +
+        scale_fill_manual(values = c('0' = "White",'1'= "Black"), labels = c("Incorrect", "Correct"), drop = FALSE) +
+        theme(plot.title = element_text(hjust = 0.5))
+
+    }
+
+  }
+
   return(forplot1)
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
