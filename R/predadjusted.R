074a0a5c7bc392502e7e363b07eeaff2f435078c
rnmontgomery <53912890+rnmontgomery@users.noreply.github.com>
2022-12-08 13:52:30 -0600

    Update predadjusted.R

--- R/predadjusted.R
+++ R/predadjusted.R
@@ -10,21 +10,17 @@
 #' @param id Id variable
 #' @param timevar Variable denoting time, only used for pre-post data
 #' @param type Type of analysis, pre-post, or group comparison
-#' @param cor Type of correlation, default is Pearson
+#' @param cor Method to estimate correlation, sigma uses the estimated covariance matrix from multivariate regression, resid, uses the residuals.
 #'
 #' @return
 #' @export
 #'
 #' @examples
 predadjusted <- function(data, variables, covariates, id, type = "group", gtvar,
-                         phi_0 = 0.50, direction, predictions,  corM = "resid"){
+                         phi_0 = 0.50, direction, predictions,  corM = "sigma"){
 
   nends <- length(variables)
-
-
-  # Need to get results
-  # Need to get weights (this is different)
-  # Need to then pass the weights and results to the test function
+  n <- dim(dataset)[1] # Assuming data set is in long format
 
   if (type == "group"){
     levels <- unique(factor(dataset[,gtvar]))
@@ -57,18 +53,62 @@ predadjusted <- function(data, variables, covariates, id, type = "group", gtvar,
           predictions <- rep(phi_0, length(variables))
         }
 
-    # So far, I've gotten the correct coefficient
-    # Next compare to increase/decrease, maybe skip mixed for now (how would difference be defined?)
-    # Then get weights, from the resids, so I need to store those actually,
-    # Then output the adjusted weights and results which can be passed to the predtest or predboot function.
-
-      } else if (corM == "other")
+      } else if (corM == "sigma")
       {
+        results <- matrix(NA,1,nends)
+        resids <- matrix(NA,dim(dataset)[1],nends)
 
+          # Building the design matrix
+          # Number of columns is 1(Intercept) + number of covariates + variables for group/pre-post
+          Xdesign <- matrix(NA,n, 1+length(covariates)+1 )
+          Xdesign[,1] <- 1
+          Xdesign[,2:(1+length(covariates))] <-  as.matrix(dataset[,covariates])
+          Xdesign[,1+length(covariates)+1] <- as.matrix(dataset[,gtvar])
 
+          # Response matrix
+          Ymat <- as.matrix(dataset[,variables])
+
+          # Beta hat matrix
+          Betahat <- solve(t(Xdesign)%*%Xdesign)%*%t(Xdesign)%*%Ymat
+
+          # Sample covariance matrix:
+          sigmahat <- (t(Ymat)%*%Ymat - t(Betahat)%*%t(Xdesign)%*%Ymat)/(n-length(covariates)-1 )
+
+          # Convert to correlation matrix
+          rhohat <- cov2cor(sigmahat)
+
+          results[1,] <- Betahat[dim(Betahat)[1],]
+          colnames(results) <- c(variables)
+          weights <- 1/rowSums(cor(Ymat)^2)
+
+          if (direction == "increase"){
+            for ( z in 1:length(variables)){
+              results[,c(variables[z])] <- ifelse(results[,c(variables[z])] > 0,1,0 )
+              predictions <- rep(phi_0, length(variables))
+            }
+          }else if (direction == "decrease"){
+            for ( z in 1:length(variables)){
+              results[,c(variables[z])] <- ifelse(results[,c(variables[z])] < 0,1,0 )
+              predictions <- rep(phi_0, length(variables))
+            }
+        }
       }
-  } else if (type == "prepost"){ # type = group
+  } else if (type == "prepost"){
 
+    if (corM == "resid"){
+
+        lmt <- lm(cbind(v1,v2,v3,v4)~age + sex + time, data = buildtestingC)
+  summary(lmt)
+
+  plot()
+
+
+
+    }else if (corM == "sigma")
+    {
+
+
+    }
 
 
   }
@@ -76,8 +116,9 @@ predadjusted <- function(data, variables, covariates, id, type = "group", gtvar,
 
 
 
-
-    outlist <- list(results,weights)
+    results <- as.vector(results)
+    weights <- as.vector(weights)
+    outlist <- list(results,weights, variables)
     return(outlist)
   }
 
