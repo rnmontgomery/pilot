09a2abf8c649c14b60e7189f16d6a7fbfcb3ed6e
Richard Vargas <131617199+gitrichardvargas@users.noreply.github.com>
2023-10-24 16:41:20 -0500

    Update predresults.R
    I've added t and wilcoxon tests as functions to be applied by create_results_vector. I took the code you had in the bottom of what you sent me and made this line:  accuracy[i] <- ifelse(p_val < phi_0, 1, 0), where accuracy is an empty vector being filled with 0s and 1s based on given predictions and p_val is the returned p value of the chosen test on the ith variable. Error handling needs some revision with so many arguments and conditions. Should be easier to do once the functions are in a separate file.

--- R/predresults.R
+++ R/predresults.R
@@ -17,7 +17,7 @@
 #' @importFrom stats wilcox.test qnorm
 #' @examples
 
-# example data frame for analysis on group data 
+# example data frame for analysis on group data
 ID2 <- 1:30
 buildtestinggroup <- as.data.frame(ID2)
 buildtestinggroup$group <- c(rep(0,15), rep(12,15))
@@ -39,7 +39,7 @@ buildtesting2$v3 <- rnorm(30, 0, 1)
 buildtesting2$v4 <- rnorm(30, 0, 1)
 
 
-# the start of the predresults function 
+# the start of the predresults function
 predresults <- function(dataset,
                         id,
                         direction,
@@ -52,113 +52,124 @@ predresults <- function(dataset,
                         phi_0 = 0.50,
                         predictions,
                         location = "median") {
-    
+
+  # error handling
+  if (!is.numeric(dataset[, gtvar])) {
+    stop(
+      "gtvar must be numeric, either a numeric variable for time or group (e.g., group 1 vs group 0)."
+    )
+  }
+
+  # NESTED FUNCTIONS
+
+  # The following functions filter the dataset into two groups
+  filter_by_group_var <- function(df, grp_var, grp_1, grp_2, vars){
+    # Subset the data by group with only the column variables mentioned in ...
+    grp_1_vars <- df[df[[grp_var]] == grp_1, vars]
+    grp_2_vars <- df[df[[grp_var]] == grp_2, vars]
+    # place the data frames into a list and return it
+    return(list(grp_1_vars = grp_1_vars, grp_2_vars = grp_2_vars))
+  }
+
+  filter_by_time_var <- function(df, id, time_var, pre, post, vars){
+    # Separates the pre and post results into separate dfs
+    grp_pre <- df[df[time_var] == pre, ]
+    grp_post <- df[df[time_var] == post, ]
+    # Place in ascending order, so that the ids are in the same row
+    # COULD USE SOME ERROR HANDLING HERE TO MAKE SURE THE IDS ARE IDENTICAL
+    sorted_grp_pre <- grp_pre[order(grp_pre[[id]]), ]
+    sorted_grp_post <- grp_post[order(grp_post[[id]]), ]
+    # Filters such that only the chosen variables in vars are present
+    # in each data frame
+    pre_vars <- sorted_grp_pre[vars]
+    post_vars <- sorted_grp_post[vars]
+    return(list(pre_vars = pre_vars, post_vars = post_vars))
+  }
+
+  # takes either the mean or median of each variable in two data frames
+  # returns a vector of the difference between the two.
+  # location of grp_2_data - location of grp_1_data
+  create_difference_vector <- function(grp_1_data, grp_2_data, location){
+    if (location == 'mean') {
+      grp_1_means <- colMeans(grp_1_data)
+      grp_2_means <- colMeans(grp_2_data)
+      differences <- grp_2_means - grp_1_means
+      return(unname(as.vector(differences)))
+    } else if (location == 'median') {
+      grp_1_medians <- apply(grp_1_data, 2, median)
+      grp_2_medians <- apply(grp_2_data, 2, median)
+      differences <- grp_2_medians - grp_1_medians
+      return(unname(as.vector(differences)))
+    }
+  }
+
+  # function to obtain the results
+  # will have added feature for mixed
+  create_results_vector <- function(hypothesis, results, diff_method = 'wilcoxon', group_a = NULL, group_b = NULL, phi_0 = .5) {
     # error handling
-    if (!is.numeric(dataset[, gtvar])) {
-        stop(
-            "gtvar must be numeric, either a numeric variable for time or group (e.g., group 1 vs group 0)."
-        )
+    if (length(hypothesis) != length(results)) stop('hypothesis and results need to be the same length')
+    if (class(results) != 'numeric') stop('results need to be numeric data type')
+    valid_hypotheses = c('increase', 'decrease', 'difference')# Valid prediction values
+    if (any(!hypothesis %in% valid_hypotheses)) {
+      stop('Invalid hypothesis detected. Valid hypotheses are: ', paste(valid_hypotheses, collapse = ', '))
     }
-    
-    # NESTED FUNCTIONS
-    
-    # The following functions filter the dataset into two groups
-    filter_by_group_var <- function(df, grp_var, grp_1, grp_2, vars){
-        # Subset the data by group with only the column variables mentioned in ...
-        grp_1_vars <- df[df[[grp_var]] == grp_1, vars]
-        grp_2_vars <- df[df[[grp_var]] == grp_2, vars]
-        # place the data frames into a list and return it
-        return(list(grp_1_vars = grp_1_vars, grp_2_vars = grp_2_vars))
+    # methods of obtaining p values
+    wilcoxon_test_p_val <- function(a, b, phi_0) wilcox.test(a, b, mu = phi_0)$p.value
+    t_test_p_val <- function(a, b) t.test(a, b)$p.value
+
+    # assignment of the user's chosen difference method to get a p value
+    p_val_test <- switch(diff_method, wilcox = wilcoxon, t = t_test)
+
+    # empty vector that will take 1s and 0s to determine accuracy of predictions
+    accuracy <- numeric(length(hypothesis))
+
+    for (i in seq_along(hypothesis)) {
+      # difference means that the user predicted that by the chosen test
+      # that there'd be a statistically significant difference for a particular variable
+      if (hypothesis[i] == 'difference') {
+        # locates the ith col variable of group_a and group_b
+        col_var_a <- group_a[[i]]
+        col_var_b <- group_b[[i]]
+        # assigns the p value from the chosen test
+        p_val = p_val_test(col_var_a, col_var_b, phi_0)
+        accuracy[i] <- ifelse(p_val < phi_0, 1, 0)
+      } else if ((hypothesis[i] == 'increase' && results[i] >= 0) || (hypothesis[i] == 'decrease' && results[i] < 0)) {
+        accuracy[i] <- 1
+      } else {
+        accuracy[i] <- 0
+      }
     }
-    
-    filter_by_time_var <- function(df, id, time_var, pre, post, vars){
-        # Separates the pre and post results into separate dfs
-        grp_pre <- df[df[time_var] == pre, ]
-        grp_post <- df[df[time_var] == post, ]
-        # Place in ascending order, so that the ids are in the same row
-        # COULD USE SOME ERROR HANDLING HERE TO MAKE SURE THE IDS ARE IDENTICAL
-        sorted_grp_pre <- grp_pre[order(grp_pre[[id]]), ]
-        sorted_grp_post <- grp_post[order(grp_post[[id]]), ]
-        # Filters such that only the chosen variables in vars are present
-        # in each data frame
-        pre_vars <- sorted_grp_pre[vars]
-        post_vars <- sorted_grp_post[vars]
-        return(list(pre_vars = pre_vars, post_vars = post_vars))
-    }
-    
-    # takes either the mean or median of each variable in two data frames
-    # returns a vector of the difference between the two.
-    # location of grp_2_data - location of grp_1_data
-    create_difference_vector <- function(grp_1_data, grp_2_data, location){
-        if (location == 'mean') {
-            grp_1_means <- colMeans(grp_1_data)
-            grp_2_means <- colMeans(grp_2_data)
-            differences <- grp_2_means - grp_1_means
-            return(unname(as.vector(differences)))
-        } else if (location == 'median') {
-            grp_1_medians <- apply(grp_1_data, 2, median)
-            grp_2_medians <- apply(grp_2_data, 2, median)
-            differences <- grp_2_medians - grp_1_medians
-            return(unname(as.vector(differences)))
-        }
-    }
-    
-    # function to obtain the results
-    # will have added feature for mixed
-    create_results_vector <- function(hypothesis, results, diff_method = 'wilcox', a = NULL, b = NULL) {
-        # error handling
-        if (length(hypothesis) != length(results)) stop('hypothesis and results need to be the same length')
-        if (class(results) != 'numeric') stop('results need to be numeric data type') 
-        valid_hypotheses = c('increase', 'decrease', 'difference')# Valid prediction values
-        if (any(!hypothesis %in% valid_hypotheses)) {
-            stop('Invalid hypothesis detected. Valid hypotheses are: ', paste(valid_hypotheses, collapse = ', '))
-        }
-        # temporary test functions 
-        # simple arithmetic will be replaced by the actual normal and wilcox tests 
-        # for now the 
-        fake_wilcox <- function(x) {
-            return(x + 0.5)
-        }
-        
-        fake_normal <- function(x) {
-            return(x + 2)
-        }
-        
-        # assignment of the user's chosen difference method to test 
-        test <- switch(diff_method, wilcox = fake_wilcox, normal = fake_normal)
-        
-        accuracy <- numeric(length(hypothesis))
-        
-        for (i in seq_along(hypothesis)) {
-            if (hypothesis[i] == 'difference') {
-                threshold <- test(results[i])
-                accuracy[i] <- ifelse(threshold > 0, 1, 0)
-            } else if ((hypothesis[i] == 'increase' && results[i] >= 0) || (hypothesis[i] == 'decrease' && results[i] < 0)) {
-                accuracy[i] <- 1
-            } else {
-                accuracy[i] <- 0
-            }
-        }
-        
-        return(accuracy)
-    }
-    
-    # assigns the filtered data frames to the variables df_a or df_b, based on type
-    if (type == "group") {
-        dfs = filter_by_group_var(dataset, gtvar, a, b, variables)
-        df_a <- dfs$grp_1_vars
-        df_b <- dfs$grp_2_vars
-    } else if (type == "prepost") {
-        dfs = filter_by_time_var(dataset, id, gtvar, a, b, variables)
-        df_a <- dfs$pre_vars
-        df_b <- dfs$post_vars
-    }
-    
-    direction_vector <- rep(direction, length(variables))
-    diff_vector <- create_difference_vector(df_a, df_b, location)
-    results <- create_results_vector(direction_vector,diff_vector)
-    list_to_plot <- list(results, diff_vector, variables)
-    return(list_to_plot)
+
+    return(accuracy)
+  }
+
+  # assigns the filtered data frames to the variables df_a or df_b, based on type
+  if (type == "group") {
+    dfs = filter_by_group_var(dataset, gtvar, a, b, variables)
+    df_a <- dfs$grp_1_vars
+    df_b <- dfs$grp_2_vars
+  } else if (type == "prepost") {
+    dfs = filter_by_time_var(dataset, id, gtvar, a, b, variables)
+    df_a <- dfs$pre_vars
+    df_b <- dfs$post_vars
+  }
+
+  direction_vector <- rep(direction, length(variables))
+  diff_vector <- create_difference_vector(df_a, df_b, location)
+  
+  if (!missing(direction)) {
+    # Calculate 'results' using 'direction_vector' and 'diff_vector'
+    results <- create_results_vector(direction_vector, diff_vector)
+  } else if (!missing(predictions)) {
+    # Calculate 'results' using 'predictions', 'diff_vector', and other arguments
+    results <- create_results_vector(predictions, diff_vector, diff_method = bound, group_a = a, group_b = b, phi_0 = phi_0)
+  } else {
+    # error handling. Needs more updates. Could be placed at the start of the function 
+    stop("Either predictions or direction must be filled.")
+  }
+  results <- create_results_vector(direction_vector,diff_vector)
+  list_to_plot <- list(results, diff_vector, variables)
+  return(list_to_plot)
 }
 # Call predresults for groups
 predresults(dataset = buildtestinggroup, direction="decrease",variables = c("v1","v2", "v3", "v4"), type="group", gtvar="group", a=0, b=12)
@@ -166,59 +177,8 @@ predresults(dataset = buildtestinggroup, direction="decrease",variables = c("v1"
 # Call predresults for pre/post
 predresults(dataset = buildtesting2, id="ID2", direction="increase", variables = c("v1", "v3"), type="prepost", gtvar="time2",a=0, b=12, location="mean")
 
+# call using the difference tests
 
-
-# this is the old code for the results vector
-# I'm keeping it commented out for further development on the mixed category
-
-# if (direction == "increase") {
-#   for (z in 1:length(variables)) {
-#     results[c(variables[z])] <-
-#       ifelse(results[c(variables[z])] > 0, 1, 0)
-#     predictions <- rep(phi_0, length(variables))
-#   }
-# } else if (direction == "decrease") {
-#   for (z in 1:length(variables)) {
-#     results[c(variables[z])] <-
-#       ifelse(results[c(variables[z])] < 0, 1, 0)
-#     predictions <- rep(phi_0, length(variables))
-#   } # The if else on 'mixed' needs to be re-examined later
-# } else if (direction == "mixed") {
-#   for (j in 1:dim(predictions)[1]) {
-#     rules <- predictions[predictions[, 1] == names(results[j]), ]
-#     if (rules[2] == "increase") {
-#       results[j] <- ifelse(results[j] > 0, 1, 0)
-#     } else if (rules[2] == "decrease") {
-#       results[j] <- ifelse(results[j] < 0, 1, 0)
-#     } else if (rules[2] == "difference") {
-#       if (bound == "wilcoxon") {
-#         split <- dataset[, c(gtvar, names(results[j]))]
-#         split1 <-
-#           split[split[, gtvar] == as.numeric(as.character(levels[1])), ]
-#         split2 <-
-#           split[split[, gtvar] == as.numeric(as.character(levels[2])), ]
-#         results[j] <-
-#           ifelse(wilcox.test(split1[, names(results[j])], split2[, names(results[j])])$p.value <
-#                    phi_0,
-#                  1,
-#                  0)
-#       } else if (bound == "normal") {
-#         split <- dataset[, c(gtvar, names(results[j]))]
-#         split1 <-
-#           split[split[, gtvar] == as.numeric(as.character(levels[1])), ]
-#         split2 <-
-#           split[split[, gtvar] == as.numeric(as.character(levels[2])), ]
-#         obdiff <-
-#           mean(split1[, names(results[j])]) - mean(split2[, names(results[j])])
-#         results[j] <-
-#           ifelse(obdiff >  qnorm(1 - (phi_0 / 2)) |
-#                    obdiff < qnorm(phi_0 / 2),
-#                  1,
-#                  0)
-#       }
-#     }
-#   }
-# }
 
 
 
