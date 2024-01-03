60aff6c64411f979b58894e06ded7cedd3c7be9a
Richard Vargas <131617199+gitrichardvargas@users.noreply.github.com>
2024-01-03 10:58:16 -0600

    Update predweights.R
    I made it more modular, less redundant and updated the RoxyGen comments to reflect the current function

--- R/predweights.R
+++ R/predweights.R
@@ -1,137 +1,57 @@
-#' Title
+# set working directory 
+# setwd('/userInfo')
+
+# import external functions and data
+source("custom_functions.R")
+source("example_data.R")
+
+# Load the package (if organized as a package)
+# devtools::load_all()
+
+# Define the predweights function
+#' Calculate weights based on correlation for a given dataset.
 #'
-#' @param dataset Data set
-#' @param variables Variables in the data set you want to include (only endpoints)
-#' @param id The ID variable from the data set
-#' @param gtvar The group or time variable from the data set
-#' @param type The type of analysis (pre-post or between groups)
-#' @param cor Type of correlation, default is pearson
+#' This function calculates weights based on correlation for either a group or
+#' prepost type of dataset.
 #'
-#' @return A list of weights, named the original variable names (type: group), or diff.variable name (type: prepost)
-#' @importFrom rlang :=
-#' @import dplyr
-#' @export
-#' @importFrom lme4 lmer
+#' @param dataset The input dataset.
+#' @param variables The variables for which to calculate weights.
+#' @param type The type of dataset ('group' or 'prepost').
+#' @param id The identifier variable (used in prepost type).
+#' @param pre A vector specifying the pre-observation time (used in prepost type).
+#' @param post A vector specifying the post-observation time (used in prepost type).
+#' @param gtvar The grouping or time variable.
+#' @param corr_method The method for calculating correlation.
+#'
+#' @return A list containing calculated weights.
+#'
 #' @examples
-#' ID <- 1:30
-#' buildtesting <- as.data.frame(ID)
-#' buildtesting$group <- c(rep(0,15), rep(1,15))
-#' buildtesting$v1 <- rnorm(30, 0, 1)
-#' buildtesting$v2 <- rnorm(30, 0, 1)
-#' buildtesting$v3 <- rnorm(30, 0, 1)
-#' buildtesting$v4 <- rnorm(30, 0, 1)
+#' # Example for "group" type
+#' predweights(dataset = buildtesting, variables = c("v1", "v2", "v3", "v4"), id = "ID2", type = "group", gtvar = "group")
 #'
-#' predweights(dataset= buildtesting, variables = c("v1", "v2", "v3", "v4"),
-#'  id = "ID2", type = "group", gtvar = "group")
+#' # Example for "prepost" type
+#' predweights(dataset = buildtesting2, variables = c("v1", "v2", "v3", "v4"), id = "ID2", type = "prepost", pre = 0, post = 12, gtvar = "time2")
 #'
-#' ID2 <- c(rep(1:15,2))
-#' buildtesting2 <- as.data.frame(ID2)
-#' buildtesting2$time2 <- c(rep(0,15), rep(12,15))
-#' buildtesting2$v1 <- rnorm(30, 0, 1)
-#' buildtesting2$v2 <- rnorm(30, 0, 1)
-#' buildtesting2$v3 <- rnorm(30, 0, 1)
-#' buildtesting2$v4 <- rnorm(30, 0, 1)
+#' # Additional examples if needed...
+
 #'
-#' predweights(dataset= buildtesting2, variables = c("v1", "v2", "v3", "v4"),
-#'  id = "ID2", type = "prepost", gtvar = "time2")
-#'
+#' @export
+predweights <- function(dataset, variables, type = "group", id=NULL, pre=NULL, post=NULL, gtvar, corr_method = "pearson") {
 
-
-predweights <- function(dataset, variables, id, type = "group", pre=NULL, post=NULL, gtvar, cor = "pearson"){
-
-    if(type == "group")
-  {
-    endpoints <- dataset[, c(variables)]
-    samplec <- cor(endpoints, method = cor)
-    weights <- 1/rowSums(samplec^2)
-
-  }else if( type == "prepost"){
-    
-    # BELOW IS THE ORIGINAL CODE WITH DPLYR 
-
-    # timevard <- dataset[,gtvar]
-    #
-    # if (!is.numeric(timevard) )
-    # {
-    #   stop("Provide a numeric 'gtvar' to indicate pre and post observations.")
-    #
-    # }
-    # if( length(unique(timevard)) > 2 ){
-    #
-    #   stop("Only pre-post data is supported with two unique time values.")
-    # }
-    #
-    # varlist <- paste0("diff.", variables)
-    #
-    # mutatefunc <- function (dataset,id,column, newname) (
-    #   dataset %>%
-    #     dplyr::group_by(!!as.name(id) ) %>%
-    #     dplyr::mutate(!!as.name(newname) := !!as.name(column) - first(!!as.name(column)))
-    #
-    # )
-    #
-    # for ( i in 1:length(variables))
-    # {
-    #   vars <- variables[i]
-    #   flist <- varlist[i]
-    #   dataset <- mutatefunc(dataset, id,  column = vars, newname = flist)
-    # }
-    # datadiffs <- dataset[dataset[,(gtvar)] == unique(timevard)[2],]
-
-
-    ### this is repetitive code and needs to be imported
-
-    filter_by_time_var <- function(df, id, time_var, pre, post, vars){
-      # Separates the pre and post results into separate dfs
-      grp_pre <- df[df[time_var] == pre, ]
-      grp_post <- df[df[time_var] == post, ]
-      # Place in ascending order, so that the ids are in the same row
-      # COULD USE SOME ERROR HANDLING HERE TO MAKE SURE THE IDS ARE IDENTICAL... do unit test
-      sorted_grp_pre <- grp_pre[order(grp_pre[[id]]), ]
-      sorted_grp_post <- grp_post[order(grp_post[[id]]), ]
-      # Filters such that only the chosen variables in vars are present
-      # in each data frame
-      pre_vars <- sorted_grp_pre[vars]
-      post_vars <- sorted_grp_post[vars]
-      return(list(pre_vars = pre_vars, post_vars = post_vars))
-    }
-
-    # takes either the mean or median of each variable in two data frames
-    # returns a vector of the difference between the two.
-    # location of grp_2_data - location of grp_1_data
-    
-    # not sure if needed, assumes location 
-    create_difference_vector <- function(grp_1_data, grp_2_data, location){
-      if (location == 'mean') {
-        grp_1_means <- colMeans(grp_1_data)
-        grp_2_means <- colMeans(grp_2_data)
-        differences <- grp_2_means - grp_1_means
-        return(unname(as.vector(differences)))
-      } else if (location == 'median') {
-        grp_1_medians <- apply(grp_1_data, 2, median)
-        grp_2_medians <- apply(grp_2_data, 2, median)
-        differences <- grp_2_medians - grp_1_medians
-        return(unname(as.vector(differences)))
-      }
-    }
-    
-    if (type == "prepost") {
-      dfs = filter_by_time_var(dataset, id, gtvar, pre, post, variables)
-      df_a <- dfs$pre_vars
-      df_b <- dfs$post_vars
-    }
-    
-    # subtracting df_a from df_b, but not their locations 
-    # if location matters then we can use the create_differnce_vector 
-    difference = df_b - df_a
-
-    # different 
-    samplec <- cor( difference, method = cor)
-
-    weights <- 1/rowSums(samplec^2)
-
+  if (type == "group") {
+    # read the article again
+    # learn what's happening here
+    endpoints <- dataset[, variables]
+    samplec <- cor(endpoints, method = corr_method)
+  } else if (type == "prepost") {
+    dfs <- filter_by_time_var(dataset, id, gtvar, pre, post, variables)
+    df_a <- dfs$pre_vars
+    df_b <- dfs$post_vars
+    pre_post_difference <- df_b - df_a
+    samplec <- cor(pre_post_difference, method = corr_method)
   }
 
+  weights <- 1/rowSums(samplec^2)
   outweight <- list(weights)
   return(outweight)
 }
